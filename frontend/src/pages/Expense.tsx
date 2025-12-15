ab3e7f4b025a998224402ea423bce703992877ba
jabir begg <jabirbegg2@gmail.com>
2025-12-15 18:49:38 +0000

    v1.2 ui

--- frontend/src/pages/Expense.tsx
+++ frontend/src/pages/Expense.tsx
@@ -58,9 +58,8 @@ const Expense: React.FC = () => {
   return (
     <div className="page">
       <Navbar />
-      <h1 className="title">Expense</h1>
-
-      <div className="remaining">
+      <div className="remaining"
+      style={fontSizeStyle}>
         🪙 Remaining budget:{" "}
         <strong style={{ color: remaining < 0 ? "red" : "green" }}>
           {currency}
@@ -149,11 +148,14 @@ const inputStyle: React.CSSProperties = {
   width: "100%",
 };
 
+const fontSizeStyle: React.CSSProperties = {
+  fontSize: "1.1rem",
+};
 const buttonStyle: React.CSSProperties = {
   padding: "0.5rem 1rem",
   borderRadius: "8px",
   border: "none",
-  background: "#4caf50",
+  background: "#88d0f1ff",
   color: "white",
   cursor: "pointer",
 };
